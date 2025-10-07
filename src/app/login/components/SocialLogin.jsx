a65b78eaa69030358596fd256ebb64b7e03e9b5c
Mahmud Hasan <mahmudhasanh35@gmail.com>
2025-10-07 13:57:53 +0600

    redirect page

--- src/app/login/components/SocialLogin.jsx
+++ src/app/login/components/SocialLogin.jsx
@@ -1,32 +1,32 @@
 "use client";
 
 import { signIn, useSession } from "next-auth/react";
-import { useRouter } from "next/navigation";
+
 import { useEffect, useRef } from "react";
 import toast from "react-hot-toast";
 import { FaFacebookF, FaGithub, FaLinkedinIn, FaGoogle } from "react-icons/fa";
 
 export default function SocialLogin({ setIsSocialLogin }) {
-  const router = useRouter();
+
   const { status } = useSession();
-  const isSocialLoginRef = useRef(false); // শুধু সোশ্যাল লগিন ট্র্যাক করবে
+  const isSocialLoginRef = useRef(false); 
 
   const handleSocialLogin = async (providerName) => {
-    isSocialLoginRef.current = true; // সোশ্যাল লগিন শুরু হচ্ছে
+    isSocialLoginRef.current = true; 
     setIsSocialLogin && setIsSocialLogin(true);
     toast.loading(`Signing in with ${providerName}...`);
-    await signIn(providerName);
+    await signIn(providerName, { callbackUrl: "/" });
   };
 
   useEffect(() => {
-    // ✅ কেবল তখনই টোস্ট দেখাবে, যখন সোশ্যাল লগিন থেকে এসেছে
+    
     if (status === "authenticated" && isSocialLoginRef.current) {
       toast.dismiss();
       toast.success("✅ Successfully logged in via social account!");
       isSocialLoginRef.current = false; // reset
-      setTimeout(() => router.push("/"), 500);
+      
     }
-  }, [status, router]);
+  }, [status]);
 
   return (
     <div className="flex justify-center space-x-4 text-xl text-gray-700">
